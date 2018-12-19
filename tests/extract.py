8042ab805ccdb963a2f22f9d4362b1f05da6e6c9
RAVI SHARMA <winmacinux@gmail.com>
2018-12-19 11:07:11 +0530

    added new states and icons

--- tests/extract.py
+++ tests/extract.py
@@ -1,30 +1,70 @@
 from bs4 import BeautifulSoup
+import os
 from urllib.request import urlretrieve
 
-def downloadImage(url, file_path, file_name):
-    fullPath = file_path +'/'+ file_name+'_icon.jpg'
-    urlretrieve(url,fullPath)
-    return
-saveFile = open('noheaders.txt','r', encoding='utf-8')
+def downloadImage(url, file_name):
+  file_path='states'
+  if not os.path.exists(file_path):
+    os.makedirs(file_path)
+  fullPath = file_path +'/'+ file_name+'_icon.jpg'
+  urlretrieve(url,fullPath)
+  return fullPath
 
+def createJsonData(stateID='', name='', imgPath='', quick=False, index=0, startFile=False, endFile=False):
+  file_path='states'
+
+  if not os.path.exists(file_path):
+    os.makedirs(file_path)
+
+  jsonFile = open(file_path+'/jsonFile.json', 'a+', encoding='utf-8')
+  
+  if startFile:
+    data = '{"cities": ['
+  elif endFile:
+    data =  ']}'
+  else:
+    index = index+1
+    data = {
+      "id": "telengana-city-"+str(index),
+      "name": name.title(),
+      "state_id": stateID,
+      "icon": imgPath,
+      "quick": quick
+    }
+
+  jsonFile.write(str(data)+",")  
+  jsonFile.close()
+  return
+
+
+saveFile = open('noheaders.txt','r', encoding='utf-8')
 html = saveFile.read()
 soup = BeautifulSoup( html, 'html.parser')
 imgs = soup.find_all('g-img', class_="BA0A6c")
-titles = soup.find_all('div', class_="kltat")
+titles = soup.find_all('div', class_="S20Xzc")
+
 print(len(imgs))
 print(len(titles))
 
-mainCities = []
-for item in range(51):
-    imageKeyURL = imgs[item].find('img').attrs.get('data-key')
-    imageSrcURL = imgs[item].find('img').attrs.get('data-src', imageKeyURL)
-    title = titles[item].find('span').string
-    print(imageSrcURL)
-    print(title)
-    title = title.strip(" ,")
-    maketrans = title.maketrans 
+createJsonData(startFile=True, endFile=False)
 
-    downloadImage(imageSrcURL, 'topcities', title.translate(maketrans(' ', '_')).lower())
+for item in range(34):
+  imageKeyURL = imgs[item].find('img').attrs.get('src')
+  # imageSrcURL = imgs[item].find('img').attrs.get('data-src', imageKeyURL)
+  titleData = titles[item].contents
+  title = ""
+  print(imageKeyURL)
+  for div in titleData:
+    title += div.string
+  print(title)
+  # title = title.strip(" ,")
+  maketrans = title.maketrans 
+
+
+  filePath = downloadImage(imageKeyURL, title.translate(maketrans(' ', '_')).lower())
+  createJsonData(stateID='36', name=title, imgPath=filePath, quick=False, index=item)
+
+createJsonData(startFile=False, endFile=True)
 
 saveFile.close()
 
